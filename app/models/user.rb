c65d0a05df1174823121adad2f30138bf0646dea
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-04-26 21:13:25 -0700

    generate a base apicontroller and api::userscontroller. able to return json block of users from the command line.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :lists
+
+  before_save { self.username = username.downcase }
+
+  validates :username, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :password, length: { minimum: 6 }, presence: true
 end
