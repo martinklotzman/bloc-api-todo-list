983042cc4725abed286f121fd85ff9dcdca2eb86
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-04-30 09:12:42 -0700

    create user, list and item api controllers

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :list
+
+  # validates :description, :list_id, presence: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
   has_many :items
+
+  # validates :name, :user_id, presence: true
 end
